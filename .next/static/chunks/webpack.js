e6e56b9e3d378f072e8fdce12d201d7738ceae5f
prottoybiswas01 <prottoybiswas575358@gmail.com>
2026-08-20 22:06:23 +0600

    build: generate Next.js production build artifacts for the store application

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f50872daa88fd95d"; }
+/******/ 		__webpack_require__.h = function() { return "71e1f28b660d7b28"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
